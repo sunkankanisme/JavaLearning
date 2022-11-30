3d4183269d5e89ab1591b3b393ff290bd40e458f
sunkangkang <sunkangkang@focusmedia.cn>
2022-11-30 08:57:39 +0800

    同步 JVM 相关笔记

---
+++ Interview/src/main/java/com/sunk/interview/Test.java
@@ -0,0 +1,16 @@
+package com.sunk.interview;
+
+import java.io.IOException;
+
+public class Test {
+
+    public static void main(String[] args) throws IOException {
+
+        final int read = System.in.read();
+
+        System.out.println(read);
+
+
+    }
+
+}

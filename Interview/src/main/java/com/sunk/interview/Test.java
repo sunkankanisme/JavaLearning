c4aaa271102d81a408117bd639e971cebe63e1aa
sunkangkang <sunkangkang@focusmedia.cn>
2022-12-01 18:06:18 +0800

    移除 Java 八股笔记

--- Interview/src/main/java/com/sunk/interview/Test.java
+++ Interview/src/main/java/com/sunk/interview/Test.java
@@ -1,16 +1,47 @@
 package com.sunk.interview;
 
 import java.io.IOException;
+import java.lang.ref.PhantomReference;
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.SoftReference;
+import java.lang.ref.WeakReference;
+import java.text.SimpleDateFormat;
+import java.util.Hashtable;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Test {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // weakReference
+        final WeakReference<Dog> weakReference = new WeakReference<>(new Dog("weakReference"));
+        // softReference
+        final SoftReference<Dog> softReference = new SoftReference<>(new Dog("softReference"));
+        // phantomReference
+        final ReferenceQueue<Dog> dogReferenceQueue = new ReferenceQueue<>();
+        final PhantomReference<Dog> phantomReference = new PhantomReference<>(new Dog("phantomReference"), dogReferenceQueue);
 
-        final int read = System.in.read();
+        final Dog hard = new Dog("HARD");
 
-        System.out.println(read);
+        System.gc();
+        System.in.read();
 
-
+        System.gc();
+        System.in.read();
     }
 
+
+    static class Dog {
+
+        private final String name;
+
+        public Dog(String name) {
+            this.name = name;
+        }
+
+        @Override
+        protected void finalize() throws Throwable {
+            super.finalize();
+            System.out.println(name + ": 我被回收了，5555~");
+        }
+    }
 }
